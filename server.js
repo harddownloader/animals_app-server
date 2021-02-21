67d688ea858c5df868adc596d737b5247585f9c4
harddownloader <super-olegpa@ukr.net>
2021-02-22 01:04:53 +0200

    add firebase

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+require('dotenv').config()
 const express = require("express")
+// firebase
+var admin = require('firebase-admin');
+
+// firebase
+var serviceAccount = require("./configs/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DB_HOST
+});
+
+console.log(defaultApp.name);
+
+// express
 const app = express()
 
 app.use(express.static("public"))
