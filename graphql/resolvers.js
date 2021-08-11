c7e4a2ab982c39495c1585d1fcd8b4e41111e5e9
harddownloader <serafimdev@gmail.com>
2021-08-12 00:02:59 +0300

    fix - graphql connection

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,21 +1,383 @@
 // src/resolvers.js
-import { PubSub } from 'graphql-subscriptions';
-// ...
+const { PubSub } = require("graphql-subscriptions");
+const Owner = require("../models/owner");
+const User = require("../models/user");
+const ownersFromBackup = require("../ownersBackUp.json");
+
 const pubsub = new PubSub(); //create a PubSub instance
-const CHANNEL_ADDED_TOPIC = 'newChannel';
-export const resolvers = {
-// ...
-Mutation: {
-    addChannel: (root, args) => {  //Create a mutation to add a new channel.
-      const newChannel = { id: String(nextId++), messages: [], name: args.name };
-      channels.push(newChannel);
-      pubsub.publish(CHANNEL_ADDED_TOPIC, { channelAdded: newChannel });  // publish to a topic
-      return newChannel;
-    }
+// const CHANNEL_ADDED_TOPIC = "newChannel";
+const resolvers = {
+  Query: {
+    currentNumber() {
+            return currentNumber;
+          },
+    // USERS
+    // get all users
+    getAllUsers: () => {
+      // return [{id: 'privet omlet'}]
+      return User.find()
+        .exec()
+        .then((docs) => {
+          console.log("getAllUsers", docs);
+          return docs;
+        })
+        .catch((err) => {
+          console.log("getAllUsers error: ", err);
+          return { error: err };
+        });
+    },
+    // get user by id
+    getUser: ({ id }) => {
+      return User.findById(id)
+        .exec()
+        .then((doc) => {
+          console.log("From database", doc);
+          if (doc) {
+            return doc;
+          } else {
+            return { message: "No valid entry found for provided ID" };
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          return { error: err };
+        });
+    },
+
+    // OWNERS
+    // get all owners
+    getAllOwners: () => {
+      return Owner.find()
+        .exec()
+        .then((docs) => {
+          console.log("getAllOwners", docs);
+          return docs;
+        })
+        .catch((err) => {
+          console.log("getAllOwners error: ", err);
+          return { error: err };
+        });
+    },
+    // get owner by id
+    getOwner: ({ id }) => {
+      return Owner.findById(id)
+        .exec()
+        .then((doc) => {
+          console.log("From database", doc);
+          if (doc) {
+            return doc;
+          } else {
+            return { message: "No valid entry found for provided ID" };
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          return { error: err };
+        });
+    },
+  },
+  Mutation: {
+    // upload owners from backup file
+    // upOwnersByBackup: () => {
+    //   for (let i = 0; i < ownersFromBackup.length; i++) {
+    //     const item = ownersFromBackup[i];
+    //     // if(i > 0) return 'first return'
+    //     const owner = new Owner({
+    //       _id: new mongoose.Types.ObjectId(),
+    //       name: item.name,
+    //       adress: item.adress,
+    //       phones: item.phone,
+    //       photoOwnerImage: item.photoOwnerImage,
+    //       photoPasportImage: item.photoPasportImage,
+    //       car: item.car,
+    //       history: item.history,
+    //       whoGave: item.whoGave,
+    //       ktoDalTel: item.ktoDalTel,
+    //       jivoder: item.jivoder,
+    //     });
+    //     owner
+    //       .save()
+    //       .then((result) => {
+    //         console.log(result);
+    //         return result;
+    //       })
+    //       .catch((err) => {
+    //         console.log(err);
+    //         // throw err;
+    //       });
+    //   }
+
+    //   return {
+    //     status: "loading owners from backup complete!",
+    //   };
+    // },
+    // add owner
+    createOwner: ({ input }) => {
+      const owner = new Owner({
+        _id: new mongoose.Types.ObjectId(),
+        name: input.name,
+        adress: input.adress,
+        phones: input.phones,
+        photoOwnerImage: input.photoOwnerImage,
+        photoPasportImage: input.photoPasportImage,
+        car: input.car,
+        history: input.history,
+        whoGave: input.whoGave,
+        ktoDalTel: input.ktoDalTel,
+        jivoder: input.jivoder,
+      });
+      return owner
+        .save()
+        .then((result) => {
+          console.log("result = ", result);
+          console.log(result._id);
+          // return { ...result._doc };
+          return result;
+        })
+        .catch((err) => {
+          console.log(err);
+          throw err;
+        });
+    },
+    // update owner fields
+    updateOwner: ({ input }) => {
+      const updateOps = {};
+      // console.log('input', input)
+      for (const ops of Object.keys(input)) {
+        updateOps[ops] = input[ops];
+      }
+      console.log("updateOps", updateOps);
+      // return false
+      return Owner.updateOne(
+        { _id: input.id },
+        {
+          $set: updateOps,
+        }
+      )
+        .exec()
+        .then((doc) => {
+          console.log("From database", doc);
+          if (doc) {
+            return doc;
+          } else {
+            return { message: "No valid entry found for provided ID" };
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          return { error: err };
+        });
+    },
+    // delete owner
+    deleteOwner: ({ id }) => {
+      return Owner.remove({ _id: id })
+        .exec()
+        .then((doc) => {
+          console.log("From database", doc);
+          if (doc) {
+            return doc;
+          } else {
+            return { message: "No valid entry found for provided ID" };
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          return { error: err };
+        });
+    },
   },
   Subscription: {
-    channelAdded: {  // create a channelAdded subscription resolver function.
-      subscribe: () => pubsub.asyncIterator(CHANNEL_ADDED_TOPIC)  // subscribe to changes in a topic
+    numberIncremented: {
+      subscribe: () => pubsub.asyncIterator(["NUMBER_INCREMENTED"]),
+    },
+  },
+};
+
+const root = {
+  // USERS
+  // get all users
+  getAllUsers: () => {
+    return User.find()
+      .exec()
+      .then((docs) => {
+        console.log("getAllUsers", docs);
+        return docs;
+      })
+      .catch((err) => {
+        console.log("getAllUsers error: ", err);
+        return { error: err };
+      });
+  },
+  // get user by id
+  getUser: ({ id }) => {
+    return User.findById(id)
+      .exec()
+      .then((doc) => {
+        console.log("From database", doc);
+        if (doc) {
+          return doc;
+        } else {
+          return { message: "No valid entry found for provided ID" };
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return { error: err };
+      });
+  },
+  addOwnerToUsersOwnersList({ userId, ownerId }) {
+    // get user information
+    // check ownerId in list
+    // если нету, то пушим и слхраняем
+
+    return;
+  },
+  deleteOwnerToUsersOwnersList({ userId, ownerId }) {
+    // берем юзера
+    // проверяем есть ли в его списке владелец с таким индификатором
+    // если есть, то удаляем и сохраняем
+    return;
+  },
+
+  // OWNERS
+  // get all owners
+  getAllOwners: () => {
+    return Owner.find()
+      .exec()
+      .then((docs) => {
+        console.log("getAllOwners", docs);
+        return docs;
+      })
+      .catch((err) => {
+        console.log("getAllOwners error: ", err);
+        return { error: err };
+      });
+  },
+  // get owner by id
+  getOwner: ({ id }) => {
+    return Owner.findById(id)
+      .exec()
+      .then((doc) => {
+        console.log("From database", doc);
+        if (doc) {
+          return doc;
+        } else {
+          return { message: "No valid entry found for provided ID" };
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return { error: err };
+      });
+  },
+  // upload owners from backup file
+  upOwnersByBackup: () => {
+    for (let i = 0; i < ownersFromBackup.length; i++) {
+      const item = ownersFromBackup[i];
+      // if(i > 0) return 'first return'
+      const owner = new Owner({
+        _id: new mongoose.Types.ObjectId(),
+        name: item.name,
+        adress: item.adress,
+        phones: item.phone,
+        photoOwnerImage: item.photoOwnerImage,
+        photoPasportImage: item.photoPasportImage,
+        car: item.car,
+        history: item.history,
+        whoGave: item.whoGave,
+        ktoDalTel: item.ktoDalTel,
+        jivoder: item.jivoder,
+      });
+      owner
+        .save()
+        .then((result) => {
+          console.log(result);
+          return result;
+        })
+        .catch((err) => {
+          console.log(err);
+          // throw err;
+        });
     }
-  }
-}
+
+    return {
+      status: "loading owners from backup complete!",
+    };
+  },
+  // add owner
+  createOwner: ({ input }) => {
+    const owner = new Owner({
+      _id: new mongoose.Types.ObjectId(),
+      name: input.name,
+      adress: input.adress,
+      phones: input.phones,
+      photoOwnerImage: input.photoOwnerImage,
+      photoPasportImage: input.photoPasportImage,
+      car: input.car,
+      history: input.history,
+      whoGave: input.whoGave,
+      ktoDalTel: input.ktoDalTel,
+      jivoder: input.jivoder,
+    });
+    return owner
+      .save()
+      .then((result) => {
+        console.log("result = ", result);
+        console.log(result._id);
+        // return { ...result._doc };
+        return result;
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err;
+      });
+  },
+  // update owner fields
+  updateOwner: ({ input }) => {
+    const updateOps = {};
+    // console.log('input', input)
+    for (const ops of Object.keys(input)) {
+      updateOps[ops] = input[ops];
+    }
+    console.log("updateOps", updateOps);
+    // return false
+    return Owner.updateOne(
+      { _id: input.id },
+      {
+        $set: updateOps,
+      }
+    )
+      .exec()
+      .then((doc) => {
+        console.log("From database", doc);
+        if (doc) {
+          return doc;
+        } else {
+          return { message: "No valid entry found for provided ID" };
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return { error: err };
+      });
+  },
+  // delete owner
+  deleteOwner: ({ id }) => {
+    return Owner.remove({ _id: id })
+      .exec()
+      .then((doc) => {
+        console.log("From database", doc);
+        if (doc) {
+          return doc;
+        } else {
+          return { message: "No valid entry found for provided ID" };
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return { error: err };
+      });
+  },
+};
+
+module.exports = resolvers
--- server.js
+++ server.js
@@ -6,7 +6,21 @@ const { ApolloServer, gql } = require("apollo-server-express");
 const { PubSub } = require("graphql-subscriptions");
 const { SubscriptionServer } = require("subscriptions-transport-ws");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
-const typeDefs = require('./graphql/schema.js');
+const typeDefs = require("./graphql/schema.js");
+const resolvers = require("./graphql/resolvers.js");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const User = require("./models/user");
+
+// mongoose
+mongoose
+  .connect(process.env.DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 (async () => {
   const PORT = 3033;
@@ -26,18 +40,34 @@ const typeDefs = require('./graphql/schema.js');
   // `;
 
   // Resolver map
-  const resolvers = {
-    // Query: {
-    //   currentNumber() {
-    //     return currentNumber;
-    //   },
-    // },
-    Subscription: {
-      numberIncremented: {
-        subscribe: () => pubsub.asyncIterator(["NUMBER_INCREMENTED"]),
-      },
-    },
-  };
+  // const resolvers = {
+  //   // Query: {
+  //   //   currentNumber() {
+  //   //     return currentNumber;
+  //   //   },
+  //   // },
+  //   Subscription: {
+  //     numberIncremented: {
+  //       subscribe: () => pubsub.asyncIterator(["NUMBER_INCREMENTED"]),
+  //     },
+  //   },
+  // };
+
+  app.get("/rest", function (req, res) {
+    return res.json({ data: "rest" });
+    // return User.find()
+    //   .exec()
+    //   .then((docs) => {
+    //     console.log("getAllUsers", docs);
+    //     // return docs;
+    //     res.json({ data: docs });
+    //   })
+    //   .catch((err) => {
+    //     console.log("getAllUsers error: ", err);
+    //     // return { error: err };
+    //     res.json({ data: "error" });
+    //   });
+  });
 
   const schema = makeExecutableSchema({ typeDefs, resolvers });
 
